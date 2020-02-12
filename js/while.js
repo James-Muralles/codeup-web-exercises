dd3b1370e7d3234fb475b8443b4b8e5d5874f6eb
James Muralles <james.muralles23@gmail.com>
2020-02-12 12:16:22 -0600

    fininshed exercises

---
+++ js/while.js
@@ -0,0 +1,34 @@
+// "use strict";
+// var i = 2;
+// while (i <= 65536) {
+//     console.log(i);
+//     i = i * 2;
+// }
+
+// This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+
+
+
+
+// var allCones = Math.floor(Math.random() * 50) +50;
+//
+// do{
+//     var conesBought = Math.floor(Math.random() * 5) + 1;
+//
+//
+//     if (conesBought < allCones) {
+//         allCones -= conesBought;
+//         console.log(conesBought + " cones sold");
+//     }else if ((conesBought > allCones) && allCones > 0){
+//         console.log("Cannot sell you " + conesBought + " I only have " + allCones);
+//     }else {
+//         allCones -= conesBought;
+//         console.log("Yay! I sold them all!");
+//     }
+// }while (allCones > 0);
+
+
+
